748a3cf0dc085f4a90d0036a80d73eea619022ec
Robin <robinsoobs942@gmail.com>
2017-10-04 12:54:13 +0200

    Company website

---
+++ Home.php
@@ -0,0 +1,218 @@
+<!Doctype html>
+<html>
+<head>
+    <link rel="icon" type="image/png" href="assets/images/DPlogo.png">
+    <title>Devprox(pty)LTD</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap Stylesheet -->
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap-css/bootstrap.css" type="text/css" >
+
+
+
+</head>
+
+<body data-spy="scroll" >
+
+<!--HOME section -->
+<section id="Home">
+
+<header class="container" role="main">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+              <img src="assets/images/devproxlogo.png" class="p-sm-0">
+
+               <ul>
+                <li><a href="#Home"><i class="fa-fa-home"></i> HOME</a></li>
+                <li><a href="#About"><i class="fa-fa-team"></i> WHAT WE DO</a></li>
+                <li><a href="#Contact"><i class="fa-fa-phone"></i> CONTACT-US</a></li>
+               </ul>
+            </div>
+    </div>
+    </nav>
+</header>
+    <div class="">
+        <div class="text-center">
+
+            <h6>We navigate the dangerous waters so you don't have to</h6>
+
+        <video id="" preload="" autoplay="true" loop="loop" muted="muted" >
+            <source src="assets/video/shark.mp4" type="video/mp4">
+            <source src="assets/video/shark.ogg" type="video/ogg">
+            <source src="assets/video/shark.webm" type="video/webm">
+        </video>
+        </div>
+    </div>
+</section>
+
+<!-- ABOUT-US section-->
+<section id="About" class="jumbotron">
+    <div>
+        <h5 class="h-25">Our Services</h5>
+    </div>
+<div>
+    <p>
+        Experts at migrating out of/integrating with Oracle service cloud and Salesforce. We have experience in various platforms / coding languages such as Java, PHP, Javascript, Python, Delphi and SQL etc. We also engage in embedded software development.
+        <br><br>
+        Devprox's team is experienced in creating tailored solutions to perfectly fit your business needs. Devprox is a team of enthusiastic and forward-thinking developers, system architects, enterprise architects, engineers, data scientists, business analysts, automated QA developers and e-commerce experts. Our skill set is diverse, however we all have one thing in common; our talent and persistent effort to contribute to the success of our clients.
+
+    </p>
+</div>
+
+    <div id="Carousel" class="carousel" >
+        <ol class="carousel-indicators">
+            <li data-target="#Carousel" data-toggle="0" class="active"></li>
+            <li data-target="#Carousel" data-toggle="1"></li>
+        </ol>
+        
+        <div class="carousel-inner" role="listbox">
+            <div class="item-active">
+                <img src="assets/images/softwareD.jpg" alt="" d>
+            </div>
+            <div class="carousel-item">
+                <img src="assets/images/javas.png" alt="">
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+
+<!-- Our Services section -->
+<section class="jumbotron-fluid">
+
+
+       <div>
+        <h1 class="">Our Services</h1>
+       </div>
+
+    <div class="col-md-5 text-lg-left">
+       <div>
+           <h6 class="text-uppercase"><i class="fa-fa-square-o"></i><strong> Easy to Customise</strong></h6>
+        <p>The fox jumps over the lazy dog</p>
+       </div>
+
+       <div>
+        <h6 class="text-uppercase"><i class="fa-fa-square-o"></i><strong> Full of Features</strong></h6>
+        <p>The fox jumps over the lazy dog</p>
+       </div>
+
+       <div>
+        <h6 class="text-uppercase"><i class="fa-fa-square-o"></i><strong> 24/7 Support.</strong></h6>
+        <p></p>
+       </div>
+
+       <div class="">
+        <h6 class="text-uppercase"><i class="fa-fa-square-o"></i><strong> All in psd files.</strong></h6>
+        <p>The fox jumps over the lazy dog</p>
+       </div>
+    </div>
+
+
+
+    <div class="col-md-5 text-lg-left form-text">
+        <div>
+            <h6 class="text-uppercase"><i class="fa-fa-square-o"></i><strong> Easy to Customise</strong></h6>
+            <p>The fox jumps over the lazy dog</p>
+        </div>
+
+        <div>
+            <h6 class="text-uppercase"><i class="fa-fa-square-o"></i><strong> Full of Features</strong></h6>
+            <p>The fox jumps over the lazy dog</p>
+        </div>
+
+        <div>
+            <h6 class="text-uppercase"><i class="fa-fa-square-o"></i><strong> 24/7 Support.</strong></h6>
+            <p></p>
+        </div>
+
+        <div class="">
+            <h6 class="text-uppercase"><i class="fa-fa-square-o"></i><strong> All in psd files.</strong></h6>
+            <p>The fox jumps over the lazy dog</p>
+        </div>
+    </div>
+</section>
+
+<!-- CONTACT-US section -->
+<section id="Contact" class="jumbotron">
+     <div class="container">
+        <div class="row">
+            <div>
+                    <h1 class="text-uppercase"><strong>Contact Us</strong></h1>
+                    <br/>
+
+
+             <form>
+                <div>
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                             <input id="username" name="username" type="text" placeholder="Name" required="" >
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                             <input id="contact" name="contact" type="number" placeholder="Phone" required="" >
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                             <input id="email" name="email" type="email" placeholder="Email" required="" >
+                        </div>
+                    </div>
+
+                    <div class="col-xl-12">
+                        <div class="form-group">
+                             <input id="message" name="message" type="text" placeholder="Message" required="" >
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                             <input id="submit" type="submit" placeholder="SEND A MESSAGE">
+                        </div>
+                    </div>
+
+                </div>
+             </form>
+            </div>
+       </div>
+     </div>
+</section>
+
+<section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div>
+                    <strong>Location</strong>
+                    <p class="t">10 Church street
+                       Unit 19
+                       San Domenico
+                       7550</p>
+                </div>
+            </div>
+        </div>
+       <div>
+        <p class="text-xl-center">Devprox(pty)LTD 2017|www.Devprox.com</p>
+       </div>
+    </footer>
+</section>
+
+<!-- Bootstrap Javascript -->
+<script src="assets/bootstrap/bootstrap-js/bootstrap.min.js"></script>
+
+<!-- Jquery plugin -->
+<script src="assets/js/jquery.js"></script>
+
+<!-- Jquery Validation -->
+<script src="assets/dist/jquery.validate.js"></script>
+<script src="assets/dist/additional-methods.js"></script>
+
+</body>
+</html>
